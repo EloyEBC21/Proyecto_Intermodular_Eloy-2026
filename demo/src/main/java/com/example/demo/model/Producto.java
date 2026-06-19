7e89f3195d32e05fb5b5b0a4382a20e29bfcd43d
Eloy Ballester SaulPC <eloyballester2@gmail.com>
2026-06-19 20:42:30 +0200

    Implementación de favicon a todo a de mas de fallos tecnicos

--- demo/src/main/java/com/example/demo/model/Producto.java
+++ demo/src/main/java/com/example/demo/model/Producto.java
@@ -1,11 +1,17 @@
 package com.example.demo.model; // Ajusta según tu paquete
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "productos") // Nombre de la tabla en la BD
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String nombre;
     private String descripcion;
     private double precio;
